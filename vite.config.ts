f4ec6be5e14b4b1927fb498e19e5647c961f5239
quoituparle <daiyue.han@outlook.com>
2025-12-04 18:16:08 +0800

    init main branch

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://api.flyflypig.online',
+        target: 'http://localhost:8000',
         changeOrigin: true, 
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
