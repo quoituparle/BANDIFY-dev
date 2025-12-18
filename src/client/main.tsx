0c2774c8124de20a3f9be7f99313aed7a3a05cb8
quoituparle <daiyue.han@outlook.com>
2025-12-18 16:13:43 +0800

    add gemini 3.0 model

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -30,8 +30,7 @@ const availableLanguages: Language[] = [
 ];
 
 const availableModels: Model[] = [
-    { id: 'gemini-2.5-flash', name: 'Gemini 2.5 Flash' },
-    { id: 'gemini-flash-latest', name: 'Gemini 2.5 Flash Latest' },
+    { id: 'gemini-3-flash-preview', name: 'Gemini 3.0 Flash' },
     { id: 'gemini-2.5-pro', name: 'Gemini 2.5 Pro' },
     { id: 'gemini-3-pro-preview', name: 'Gemini 3.0 Pro'}
 ];
